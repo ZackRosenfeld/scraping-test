a44030d32d0ddb7650dbe69a9ef955202a72ab67
ZackRosenfeld <zackrosenfeld3@gmail.com>
2024-09-03 20:22:34 -0500

    Main Commit (#4)

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_h('Dave')
+    print_hi('William')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
